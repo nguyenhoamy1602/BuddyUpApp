ad97586d030ef072eab859340baf5845b4abd27f
Gabrielle Chan <salufie@gmail.com>
2018-02-17 16:26:32 +1100

    Added Registration with Firebase

--- Swift/BuddyUpApp/SignUpViewController.swift
+++ Swift/BuddyUpApp/SignUpViewController.swift
@@ -7,9 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBAction func createAccount(_ sender: Any) {
+        if let email = email.text, let password = password.text{
+            Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                if error != nil{//login error
+                    let alert = UIAlertController(title: "Error", message: "Cannot create an account", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    
+                }else{//successful ogin
+                    
+                    print("Success")
+                    self.performSegue(withIdentifier: "goToInitial", sender: self)
+                    
+                }
+            })
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

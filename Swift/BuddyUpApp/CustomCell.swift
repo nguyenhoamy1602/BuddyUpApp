deb5d7372a92554d2b68b658e922df3d2b895c78
Gabrielle Chan <salufie@gmail.com>
2018-02-25 17:17:58 +1100

    Small fixes

---
+++ Swift/BuddyUpApp/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  BuddyUpApp
+//
+//  Created by Gabrielle Chandrasaputra on 18/2/18.
+//  Copyright © 2018 Arizec. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
---
+++ Swift/BuddyUpApp/TimetableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TimetableViewController.swift
+//  BuddyUpApp
+//
+//  Created by Gabrielle Chandrasaputra on 18/2/18.
+//  Copyright © 2018 Arizec. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class TimetableViewController: UIViewController {
+    let formatter = DateFormatter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
+extension TimetableViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2018 01 01")
+        let endDate = formatter.date(from: "2018 12 31")
+        
+        let parameters = ConfigurationParameters(startDate: startDate!, endDate: endDate!)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomerCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+}
